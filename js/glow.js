335e8116cd87b1f7b5305cdb1cdb2d3b94caef26
Lucas <lucas.fafx@gmail.com>
2024-02-22 11:41:21 -0300

    update glow.js

--- js/glow.js
+++ js/glow.js
@@ -8,3 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
         glow.style.display = 'block'; // Mostrar o elemento de brilho
     });
 });
+
+// Captura o evento de rolagem dentro do iframe
+document.querySelector('.neon iframe').contentWindow.addEventListener('scroll', function(event) {
+    var iframe = document.querySelector('.neon iframe');
+    var scrollTop = iframe.contentWindow.pageYOffset || iframe.contentDocument.documentElement.scrollTop || iframe.contentDocument.body.scrollTop || 0;
+    var scrollLeft = iframe.contentWindow.pageXOffset || iframe.contentDocument.documentElement.scrollLeft || iframe.contentDocument.body.scrollLeft || 0;
+
+    // Se a rolagem estiver no topo ou no fundo do iframe
+    if (scrollTop === 0 || scrollTop + iframe.clientHeight >= iframe.contentDocument.documentElement.scrollHeight) {
+        // Impede a propagação da rolagem para a página principal
+        event.preventDefault();
+    }
+});
